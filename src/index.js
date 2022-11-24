8b426746e757814b71b9f8eb721d591479acf447
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2022-11-24 14:29:50 +0530

    handling header using js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles/main.scss'
+
+
+const header = document.getElementById("header");
+
+document.getElementById('hamburger').addEventListener('click', () => {
+    header.classList.toggle('mobile-nav--expand');
+})
+
+
+
+
+document.addEventListener('scroll', (e) => {
+    let lastKnownScrollPosition = window.scrollY;
+    if (lastKnownScrollPosition >= 100){
+        header.classList.add('header--sticky');
+    }else if(lastKnownScrollPosition == 0){
+        header.classList.remove('header--sticky');
+    }
+})
