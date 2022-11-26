7d0c07a043735ff441e57af77f62d4f269f4f130
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2022-11-26 15:03:17 +0530

    using reduce css properly

--- webpack.config.js
+++ webpack.config.js
@@ -92,9 +92,10 @@ module.exports = {
             // {from:'./dist/index.html', to:'../index.html'},
         ]}),
         new MiniCssExtractPlugin(),
-        // new PurgeCSSPlugin({
-        //     paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-        //   }),
+        new PurgeCSSPlugin({
+            paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+            only: ['bundle',]
+          }),
         new ImageminWebpWebpackPlugin({
             config: [{
               test: /\.(jpe?g|png)/,
